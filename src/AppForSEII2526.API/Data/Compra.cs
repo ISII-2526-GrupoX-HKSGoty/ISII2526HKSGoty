c76716b45dc17d98a8a4b790db59356021f93bc1
fernando <Fernando.Martinez22@alu.uclm.es>
2025-10-04 09:07:44 +0200

    Creacion de la Clase Compra y de sus atributos menos BocadillosComprados y Metodo_Pago

---
+++ src/AppForSEII2526.API/Data/Compra.cs
@@ -0,0 +1,22 @@
+﻿namespace AppForSEII2526.API.Data
+{
+    public class Compra
+    {
+        [StringLength(20, ErrorMessage = "Maximum 50, minimum 10", MinimumLength = 5)]
+        public string Apellido_1Cliente { get; set; }
+
+        [StringLength(20, ErrorMessage = "Maximum 50, minimum 10", MinimumLength = 5)]
+        public string Apellido_2Cliente { get; set; }
+
+        public int Id { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public int nBocadillos { get; set; }
+
+        [StringLength(20, ErrorMessage = "Maximum 50, minimum 10", MinimumLength = 5)]
+        public string NombreCliente { get; set; }
+
+        [Precision(10, 2)]
+        public decimal PrecioTotal { get; set; }
+
+    }
+}
