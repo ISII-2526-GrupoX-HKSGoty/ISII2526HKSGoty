2ac33555b534e6cf3eb1e951666a5b0802a41f48
fernando <Fernando.Martinez22@alu.uclm.es>
2025-10-04 17:42:56 +0200

    correción

--- src/AppForSEII2526.API/Data/Compra.cs
+++ src/AppForSEII2526.API/Data/Compra.cs
@@ -2,17 +2,17 @@
 {
     public class Compra
     {
-        [StringLength(20, ErrorMessage = "Maximum 50, minimum 10", MinimumLength = 5)]
+        [StringLength(20, ErrorMessage = "Maxímo 50, minimo 10", MinimumLength = 5)]
         public string Apellido_1Cliente { get; set; }
 
-        [StringLength(20, ErrorMessage = "Maximum 50, minimum 10", MinimumLength = 5)]
+        [StringLength(20, ErrorMessage = "Maxímo 50, minimo 10", MinimumLength = 5)]
         public string Apellido_2Cliente { get; set; }
 
         public int Id { get; set; }
         public DateTime FechaCompra { get; set; }
         public int nBocadillos { get; set; }
 
-        [StringLength(20, ErrorMessage = "Maximum 50, minimum 10", MinimumLength = 5)]
+        [StringLength(20, ErrorMessage = "Maxímo 50, minimo 10", MinimumLength = 5)]
         public string NombreCliente { get; set; }
 
         [Precision(10, 2)]
