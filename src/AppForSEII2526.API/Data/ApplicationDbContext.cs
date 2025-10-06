0f18c987622a1b298c31ba7257062468a96e3581
yo2727 <miguel.cifuentes@alu.uclm.es>
2025-10-06 11:20:43 +0200

    14 clases cu comprar bono
    * Creadas las clases BonoBocadillo, BonosComprados, CompraBono, TipoBocadillos con los atributos y metodos necesarios.
    
    * Relaciones añadidas y cambios en los constructores y metodos equals y hash
    
    * añadidos los constructores vacios y añadidas las clases al dbContext
    
    * añadidos los atributos y constructores a la clase applicationUser
    
    * Metodo pago anidado a CompraBono
    
    * Creación diagrama de clases

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -4,5 +4,11 @@ using AppForSEII2526.API.Models;
 
 namespace AppForSEII2526.API.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+    public DbSet<BonoBocadillo> BonoBocadillos{get; set;}
+    public DbSet<BonosComprados> BonosComprados { get; set; }
+    public DbSet<CompraBono> CompraBono { get; set; }
+    public DbSet<TipoBocadillo> TipoBocadillo { get; set; }
+
 }
