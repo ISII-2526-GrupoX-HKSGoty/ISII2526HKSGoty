b99218d04c5f1892fefc393174e1a87f04eb9316
MIGUEL CIFUENTES GARCÍA <Miguel.Cifuentes@alu.uclm.es>
2025-10-06 17:54:51 +0200

    soulcion conflictos generación migraciones en la clase CompraBocadillo

--- src/AppForSEII2526.API/Models/CompraBocadillo.cs
+++ src/AppForSEII2526.API/Models/CompraBocadillo.cs
@@ -1,41 +1,39 @@
 ﻿
+
 namespace AppForSEII2526.API.Models
 {
     [PrimaryKey(nameof(BocadilloId), nameof(CompraId))]
     public class CompraBocadillo
     {
+        public Bocadillo Bocadillo { get; set; }
         public int BocadilloId { get; set; }
         public int Cantidad { get; set; }
-
+        public Compra Compra { get; set; }
         public int CompraId { get; set; }
         public string NombreBocadillo { get; set; }
         [Precision(10, 2)]
-        public decimal Precio { get; set; }
-        public Bocadillo Bocadillo { get; set; }
-        public Compra Compra { get; set; }
-        public TipoPan Tipopan { get; set; }
-        
+        public decimal Precio { get; set; }        
 
         public CompraBocadillo()
         {
-            Bocadillo = new Bocadillo();
-            Compra = new Compra();
+            
         }
+
         public override bool Equals(object? obj)
         {
             return obj is CompraBocadillo bocadillo &&
+                   EqualityComparer<Bocadillo>.Default.Equals(Bocadillo, bocadillo.Bocadillo) &&
                    BocadilloId == bocadillo.BocadilloId &&
                    Cantidad == bocadillo.Cantidad &&
+                   EqualityComparer<Compra>.Default.Equals(Compra, bocadillo.Compra) &&
                    CompraId == bocadillo.CompraId &&
                    NombreBocadillo == bocadillo.NombreBocadillo &&
-                   Precio == bocadillo.Precio &&
-                   EqualityComparer<Bocadillo>.Default.Equals(Bocadillo, bocadillo.Bocadillo) &&
-                   EqualityComparer<Compra>.Default.Equals(Compra, bocadillo.Compra);
-        }
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(BocadilloId, Cantidad, CompraId, NombreBocadillo, Precio, Bocadillo, Compra);
+                   Precio == bocadillo.Precio;
         }
 
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Bocadillo, BocadilloId, Cantidad, Compra, CompraId, NombreBocadillo, Precio);
+        }
     }
 }
