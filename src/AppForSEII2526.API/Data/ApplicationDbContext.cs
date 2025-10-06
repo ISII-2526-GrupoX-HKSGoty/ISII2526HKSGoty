dba4ebf87c02f7484b915a9182779eb093a07a9e
fernando <Fernando.Martinez22@alu.uclm.es>
2025-10-06 10:50:06 +0200

    generar diagrama de clases y prueba de migraciones

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -7,6 +7,6 @@ namespace AppForSEII2526.API.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
     public DbSet<Bocadillo> Bocadillos { get; set; }
     public DbSet<Compra> Compras { get; set; }
-    public DbSet<ComprarBocadillo> ComprarBocadillos { get; set; }
+    public DbSet<CompraBocadillo> ComprarBocadillos { get; set; }
     public DbSet<TipoPan> tipoPan { get; set; }
 }
