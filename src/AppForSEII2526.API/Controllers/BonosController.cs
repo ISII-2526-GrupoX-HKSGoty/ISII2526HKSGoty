2ea19f88c8db6554e10bfc40f85679e5db32a025
MIGUEL CIFUENTES GARCÍA <Miguel.Cifuentes@alu.uclm.es>
2025-10-24 10:41:13 +0200

    Creado el DTO y controlador para GET bonos

---
+++ src/AppForSEII2526.API/Controllers/BonosController.cs
@@ -0,0 +1,42 @@
+﻿using AppForSEII2526.API.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BonosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<BonosController> _logger;
+
+        public BonosController(ApplicationDbContext context, ILogger<BonosController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(GET_Bono_DTO), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult> GetBonos(string? nombre, string? tipo)
+        {
+            if(_context.BonoBocadillos == null)
+            {
+                _logger.LogError("no existe la tabla");
+                return NotFound();
+            }
+
+            var bonos = await _context.BonoBocadillos
+                .Include(b => b.TipoBocadillo)
+                .Where(b=>((b.nombre.Contains(nombre)) || nombre == null) &&
+                          ((b.TipoBocadillo.nombreTipo.Contains(tipo)) || tipo == null))
+                .Select(b=> new GET_Bono_DTO(b.nombre, b.PVP, b.nBocadillos, b.TipoBocadillo))
+                .ToListAsync();
+            return Ok(bonos);
+
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/GET_Bono_DTO.cs
@@ -0,0 +1,37 @@
+﻿
+namespace AppForSEII2526.API.DTOs
+{
+    public class GET_Bono_DTO
+    {
+        public GET_Bono_DTO(string nombre, double PVP, int numero, TipoBocadillo tipo) {
+
+            this.nombre = nombre;
+            this.PVP = PVP;
+            this.numero = numero;
+            this.tipo = tipo;
+
+        }
+
+        public string nombre { get; set; }
+
+        public double PVP { get; set; }
+
+        public int numero { get; set; }
+
+        public TipoBocadillo tipo { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj  is GET_Bono_DTO dTO &&
+                   nombre == dTO.nombre &&
+                   PVP == dTO.PVP &&
+                   numero == dTO.numero &&
+                   tipo == dTO.tipo;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(nombre, PVP, numero, tipo);
+        }
+    }
+}
