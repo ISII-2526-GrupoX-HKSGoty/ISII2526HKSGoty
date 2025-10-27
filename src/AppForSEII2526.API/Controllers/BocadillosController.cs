25dc8655ba32ccc5e1887b323996ab452a3cb4a8
Amine Saghrouchni Yosufi <amine.saghrouchni@alu.uclm.es>
2025-10-27 13:54:13 +0100

    Creación del DTO y del GET

--- src/AppForSEII2526.API/Controllers/BocadillosController.cs
+++ src/AppForSEII2526.API/Controllers/BocadillosController.cs
@@ -1,8 +1,38 @@
-﻿namespace AppForSEII2526.API.Controllers
+﻿using AppForSEII2526.API.DTOs;
+using AppForSEII2526.API.Models;
+
+namespace AppForSEII2526.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class BocadillosController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<BocadillosController> _logger;
+
+        public BocadillosController(ApplicationDbContext context, ILogger<BocadillosController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(List<GET_CrearResenaDTO>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetBocadillosResenya(string? nombre, Tamaño? tamaño, string? tipoPan, float? PVP)
+        {
+            IList<GET_CrearResenaDTO> bocadillos = await _context.Bocadillos
+                .Include(b => b.tipoPan)
+                .Where(b =>
+                    (nombre == null || b.nombre.Contains(nombre)) &&
+                    ((!tamaño.HasValue || b.tamaño == tamaño.Value)) &&
+                    (tipoPan == null || b.tipoPan.Nombre.Contains(tipoPan)) &&
+                    (!PVP.HasValue || (float) b.PVP <= PVP.Value)
+                )
+                .Select(b => new GET_CrearResenaDTO(b.nombre, b.PVP, b.tipoPan, b.tamaño))
+                .ToListAsync();
+            return Ok(bocadillos);
+        }
     }
 }
---
+++ src/AppForSEII2526.API/DTOs/GET_CrearResenaDTO.cs
@@ -0,0 +1,36 @@
+﻿
+namespace AppForSEII2526.API.DTOs
+{
+    public class GET_CrearResenaDTO
+    {
+
+        public GET_CrearResenaDTO(string nombre, decimal pVP, TipoPan tipoPan, Tamaño tamaño)
+        {
+            this.nombre = nombre;
+            PVP = pVP;
+            this.tipoPan = tipoPan;
+            this.tamaño = tamaño;
+        }
+
+        public string nombre { get; set; }
+        public decimal PVP { get; set; }
+        public TipoPan tipoPan { get; set; }
+
+        public Tamaño tamaño { get; set; }
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is GET_CrearResenaDTO dTO &&
+                   nombre == dTO.nombre &&
+                   PVP == dTO.PVP &&
+                   EqualityComparer<TipoPan>.Default.Equals(tipoPan, dTO.tipoPan) &&
+                   tamaño == dTO.tamaño;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(nombre, PVP, tipoPan, tamaño);
+        }
+    }
+}
