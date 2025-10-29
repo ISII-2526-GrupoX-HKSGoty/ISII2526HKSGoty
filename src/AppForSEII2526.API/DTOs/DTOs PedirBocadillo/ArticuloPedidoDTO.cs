59ea3d9a4515a29a67824de3fab6faf1658915a3
fernando <Fernando.Martinez22@alu.uclm.es>
2025-10-29 15:51:40 +0100

    DTOs y solucion de errores

---
+++ src/AppForSEII2526.API/DTOs/DTOs PedirBocadillo/ArticuloPedidoDTO.cs
@@ -0,0 +1,39 @@
+﻿
+namespace AppForSEII2526.API.DTOs.DTOs_PedirBocadillo
+{
+    public class ArticuloPedidoDTO
+    {
+        public string nombreBocadillo { get; set; }
+
+        [Required]
+        public int Cantidad { get; set; }
+        public int PVP { get; set; }
+        public string TipoPan { get; set; }
+
+        public ArticuloPedidoDTO()
+        {
+        }
+
+        public ArticuloPedidoDTO(string nombreBocadillo, int cantidad, int pVP, string tipoPan)
+        {
+            this.nombreBocadillo = nombreBocadillo;
+            Cantidad = cantidad;
+            PVP = pVP;
+            TipoPan = tipoPan;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ArticuloPedidoDTO dTO &&
+                   nombreBocadillo == dTO.nombreBocadillo &&
+                   Cantidad == dTO.Cantidad &&
+                   PVP == dTO.PVP &&
+                   TipoPan == dTO.TipoPan;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(nombreBocadillo, Cantidad, PVP, TipoPan);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/DTOs PedirBocadillo/CrearPedidoDTO.cs
@@ -0,0 +1,47 @@
+﻿using static AppForSEII2526.API.Models.CompraBono;
+
+namespace AppForSEII2526.API.DTOs.DTOs_PedirBocadillo
+{
+    public class CrearPedidoDTO
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor introduzca su nombre")]
+        public string nombre { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor elija el metodo de pago")]
+        public MetodoPago Metodo_Pago { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor introduzca su primer apellido")]
+        public string apellido1 { get; set; }
+        public string? apellido2 { get; set; }
+        public IList<ArticuloPedidoDTO> ArticuloPedido { get; set; }
+
+
+        public CrearPedidoDTO(string nombre, MetodoPago metododepago, string apellido1, string apellido2, IList<ArticuloPedidoDTO> articulopedido)
+        {
+            this.nombre = nombre;
+            Metodo_Pago = metododepago;
+            this.apellido1 = apellido1;
+            this.apellido2 = apellido2;
+            ArticuloPedido = articulopedido;
+        }
+
+        public CrearPedidoDTO()
+        {
+            ArticuloPedido = new List<ArticuloPedidoDTO>();
+        }
+        public override bool Equals(object? obj)
+        {
+            return obj is CrearPedidoDTO dTO &&
+                   nombre == dTO.nombre &&
+                   Metodo_Pago == dTO.Metodo_Pago &&
+                   apellido1 == dTO.apellido1 &&
+                   apellido2 == dTO.apellido2 &&
+                   EqualityComparer<IList<ArticuloPedidoDTO>>.Default.Equals(ArticuloPedido, dTO.ArticuloPedido);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(nombre, Metodo_Pago, apellido1, apellido2, ArticuloPedido);
+        }
+    }
+}
