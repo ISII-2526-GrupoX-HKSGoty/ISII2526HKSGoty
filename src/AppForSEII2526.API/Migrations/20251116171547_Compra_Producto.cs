8cd4b6b68cd16884552b75c472fa22fc7960d198
JavierCalleja <JavierLaureano.Calleja@alu.uclm.es>
2025-11-16 14:13:42 +0100

    migracion/modificaciontests

---
+++ src/AppForSEII2526.API/Migrations/20251116171547_Compra_Producto.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppForSEII2526.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Compra_Producto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TipoProductoId = table.Column<int>(type: "int", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Productos_TipoProductos_TipoProductoId",
+                        column: x => x.TipoProductoId,
+                        principalTable: "TipoProductos",
+                        principalColumn: "TipoProductoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Compra_Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdProducto = table.Column<int>(type: "int", nullable: false),
+                    IdCompra = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    PrecioUnitario = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Compra_Productos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Compra_Productos_Compras_IdCompra",
+                        column: x => x.IdCompra,
+                        principalTable: "Compras",
+                        principalColumn: "CompraId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Compra_Productos_Productos_IdProducto",
+                        column: x => x.IdProducto,
+                        principalTable: "Productos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Producto_Compras",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductoId = table.Column<int>(type: "int", nullable: false),
+                    CompraId = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    PVP = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producto_Compras", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Producto_Compras_Compras_CompraId",
+                        column: x => x.CompraId,
+                        principalTable: "Compras",
+                        principalColumn: "CompraId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Producto_Compras_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compra_Productos_IdCompra",
+                table: "Compra_Productos",
+                column: "IdCompra");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compra_Productos_IdProducto",
+                table: "Compra_Productos",
+                column: "IdProducto");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Producto_Compras_CompraId",
+                table: "Producto_Compras",
+                column: "CompraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Producto_Compras_ProductoId",
+                table: "Producto_Compras",
+                column: "ProductoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_TipoProductoId",
+                table: "Productos",
+                column: "TipoProductoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Compra_Productos");
+
+            migrationBuilder.DropTable(
+                name: "Producto_Compras");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
