2cd734a1b9f0907fcf36b936f2dcb7af9b1446b5
MIGUEL CIFUENTES GARCÍA <Miguel.Cifuentes@alu.uclm.es>
2026-05-06 12:01:07 +0200

    Creado state container

---
+++ src/AppForSEII2526.Web/CompraBonoStateContainer.cs
@@ -0,0 +1,69 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class CompraBonoStateContainer
+    {
+
+        //we create an instance of Rental when an instance of RentalStateContainer is created
+        public CompraBonoDTO Compra { get; private set; } = new CompraBonoDTO()
+        {
+            Items = new List<ItemBonoDTO>()
+        };
+
+        //we compute the TotalPrice of the movies we have selected for renting them
+        public double TotalPrice
+        {
+            get
+            {
+                return Convert.ToDouble(Compra.Items.Sum(ri => ri.Pvp * ri.Cantidad));
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+
+        public void AddBocadilloParaComprar(ItemBonoDTO bono)
+        {
+            //before adding a movie we checked whether it has been already added
+            if (!Compra.Items.Any(ri => ri.NombreBono == bono.NombreBono))
+                //we add it if it is not in the list
+                Compra.Items.Add(new ItemBonoDTO()
+                {
+                    Pvp = bono.Pvp,
+                    NBocadillos = bono.NBocadillos,
+                    NombreBono = bono.NombreBono,
+                    Cantidad = bono.Cantidad,
+                    TipoBocadillo = bono.TipoBocadillo
+
+                });
+        }
+
+        //to delete movies from the list of selected movies
+        public void RemoveCompraArticuloParaComprar(ItemBonoDTO item)
+        {
+            Compra.Items.Remove(item);
+        }
+
+        //we eliminate all the movies from the list
+        public void LimpiarCarroCompra()
+        {
+            Compra.Items.Clear();
+        }
+
+        //we have already finished the process of renting, thus, we create a new Rental 
+        public void CompraProcessed()
+        {
+            //we have finished the rental process so we create a new object without data
+            Compra = new CompraBonoDTO()
+            {
+                Items = new List<ItemBonoDTO>()
+            };
+        }
+    }
+}
+
+
--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -45,6 +45,8 @@ string? URI2API = builder.Configuration.GetValue(typeof(string), "AppForSEII2526
 
 builder.Services.AddScoped<AppForSEII2526APIClient>(sp => new AppForSEII2526APIClient(URI2API, new HttpClient()));
 
+builder.Services.AddScoped<CompraBonoStateContainer>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
