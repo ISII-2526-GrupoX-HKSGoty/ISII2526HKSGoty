e424ab495b73a60612d4819595fab8fdfa51b9e4
fernando <Fernando.Martinez22@alu.uclm.es>
2026-05-17 20:07:54 +0200

    solucion de bugs en el sql de TipoBocadillo

--- src/AppForSEII2526.API/Data/dbo.TipoBocadillo.data.sql
+++ src/AppForSEII2526.API/Data/dbo.TipoBocadillo.data.sql
@@ -1,6 +1,6 @@
 SET IDENTITY_INSERT [dbo].[TipoBocadillo] ON
-INSERT INTO [dbo].[TipoBocadillo] ([idTipo], [nombreTipo]) VALUES (1, N'veganos')
-INSERT INTO [dbo].[TipoBocadillo] ([idTipo], [nombreTipo]) VALUES (2, N'vegetarianos')
+INSERT INTO [dbo].[TipoBocadillo] ([idTipo], [nombreTipo]) VALUES (1, N'vegano')
+INSERT INTO [dbo].[TipoBocadillo] ([idTipo], [nombreTipo]) VALUES (2, N'vegetariano')
 INSERT INTO [dbo].[TipoBocadillo] ([idTipo], [nombreTipo]) VALUES (3, N'sin gluten')
 INSERT INTO [dbo].[TipoBocadillo] ([idTipo], [nombreTipo]) VALUES (4, N'normal')
 SET IDENTITY_INSERT [dbo].[TipoBocadillo] OFF
